dc272a55c90a08bc88efc7c013a3cf01f26eb4d5
Nihal Singh <singhn77@mcmaster.ca>
2023-09-20 18:21:48 -0400

    website is finished

---
+++ websitescript.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    // we're gonna target an element on our webpage and using that element
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
